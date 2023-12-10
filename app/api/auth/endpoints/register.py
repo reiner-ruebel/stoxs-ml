2953aad25af8d6e914855c8672f2cab284eb04d4
Reiner Rübel <reiner.ruebel@corvendor.com>
2023-12-10 13:32:17 +0100

    perfect pw check

--- app/api/auth/endpoints/register.py
+++ app/api/auth/endpoints/register.py
@@ -27,37 +27,26 @@ def register() -> Response_c:
         if json_data is None:
             return create_response_c("No data provided", ok = False)
 
-        data: dict[str, Any] = cast(dict[str, Any], json_data)
-        register_data: RegisterModel = RegisterSchema().load(data)
+        register_raw_data: dict[str, Any] = cast(dict[str, Any], json_data)
+        register_model: RegisterModel = RegisterSchema().load(register_raw_data)
 
-        if user_exist(register_data.username, register_data.email):
+        if user_exist(register_model.username, register_model.email):
             return create_response_c("Username or user mail already exists", ok = False)
         
-        # https://flask-security-too.readthedocs.io/en/stable/api.html#flask_security.password_complexity_validator usage not 100% clear
-        password: Optional[str] = data.get('password')
-        if (password is None):
-            return create_response_c("No password provided", ok = False)
-
-        data_password_removed: dict = data.copy()
-        data_password_removed.pop('password', None)
-
-        validation_msgs: Optional[list[str]] = user_policy_checker(password, register_data.username, register_data.email)
+        validation_msgs: Optional[list[str]] = user_policy_checker(register_model.password, register_model.username, register_model.email)
 
         if validation_msgs:
             return create_response_c(validation_msgs, ok = False)
 
-        validation_msgs = password_complexity_validator(password, is_register = True, **data_password_removed)
+        validation_msgs = password_complexity_validator(register_model.password, is_register = True)
 
         if validation_msgs:
             return create_response_c(validation_msgs, ok = False)
 
-        password = cast(str, hash_password(password))
-        data["password"] = password
-        del data_password_removed # Now, we are in control that the original pw is not accessible any more (by standard code)
+        register_raw_data["password"] = cast(str, hash_password(register_model.password))
+        register_raw_data["active"] = False # user needs to confirm email address first
         
-        data["active"] = False # user needs to confirm email address first
-        
-        user = security.datastore.create_user(**data)
+        user = security.datastore.create_user(**register_raw_data)
         user.save()
 
         return create_response_c("Registration mail sent. Please confirm to finish your registration.")
